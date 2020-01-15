09bf2ae1f47fe6dd428cdd9716f4da0ca2ce1075
José Carlos <jcolscript@gmail.com>
2020-01-15 01:01:51 -0300

    adicionando o componente do editor ao App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,26 @@
+<template>
+  <div id="app">
+    <editor />
+  </div>
+</template>
 
+<script>
+import Editor from "./components/Editor.vue";
+
+export default {
+  name: "App",
+  components: {
+    Editor
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
