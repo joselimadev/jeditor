2667debfddb938ca97ef2a8e1332c911a59dc8e8
José Carlos <jcolscript@gmail.com>
2020-01-15 00:57:06 -0300

    adicionando o plugin do quill no Editor.vue

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,38 @@
 <template>
-  <div class="editor"></div>
+  <div class="editor">
+    <quill-editor v-model="content" :options="editorOption"></quill-editor>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Editor",
+  data() {
+    return {
+      content: "",
+      editorOption: {}
+    };
+  }
+};
+</script>
 
-<style></style>
+<style>
+.quill-editor,
+.quill-code {
+  width: 100%;
+  height: 90vh;
+}
+
+.quill-code {
+  height: auto;
+  border: none;
+}
+.quill-code > .title {
+  border: 1px solid #ccc;
+  border-left: none;
+  height: 3em;
+  line-height: 3em;
+  text-indent: 1rem;
+  font-weight: bold;
+}
+</style>
