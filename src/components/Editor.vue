57ea5166628ec05e3227e45a384c52ad3b16b839
José Carlos <jcolscript@gmail.com>
2020-01-15 01:06:23 -0300

    configurando o toolbar do editor e lint

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="editor">
-    <quill-editor v-model="content" :options="editorOption"></quill-editor>
+    <quill-editor
+      v-model="content"
+      :options="editorOption"
+    />
   </div>
 </template>
 
@@ -10,7 +13,28 @@ export default {
   data() {
     return {
       content: "",
-      editorOption: {}
+      editorOption: {
+        modules: {
+          toolbar: {
+            container: [
+              ["bold", "italic", "underline", "strike"],
+              ["blockquote", "code-block"],
+              [{ header: 1 }, { header: 2 }],
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ script: "sub" }, { script: "super" }],
+              [{ indent: "-1" }, { indent: "+1" }],
+              [{ direction: "rtl" }],
+              [{ size: ["small", false, "large", "huge"] }],
+              [{ header: [1, 2, 3, 4, 5, 6, false] }],
+              [{ font: [] }],
+              [{ color: [] }, { background: [] }],
+              [{ align: [] }],
+              ["clean"],
+              ["link", "image", "video"]
+            ]
+          }
+        }
+      }
     };
   }
 };
